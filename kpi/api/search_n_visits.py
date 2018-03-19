899f4b4693e5acb8be224d59eb6445cc796a67c8
MayukhSobo <mayukh2012@hotmail.com>
2018-03-19 16:45:18 +0530

    Adding one more API for seller portfolio

--- kpi/api/search_n_visits.py
+++ kpi/api/search_n_visits.py
@@ -107,7 +107,6 @@ def timely_visits(typ, time_aggregation, filter_col=None):
         ret = ret.mean().reset_index()
         ret.columns = ['property_id', 'visit_count']
         output = lambda: ret.set_index('property_id')
-    
     if Constants.DEBUG:
         import sys
         jwrap = json_wrap(override=sys.stdout)
@@ -204,45 +203,51 @@ def past_search_per(what, filter_col=None):
             return True, dwrap.wrap(functor=output)
 
 
-def search_quality(what, miss_rate=False):
-    if miss_rate:
-        prop_feat_list = data_property[what].apply(
-            lambda x: x.split('|')).values
-        lut = dict(list(zip(data_property.id.values, prop_feat_list)))
-        from collections import defaultdict
+def search_quality(what, missing_rate=False):
+    """
+    Section 8: Seller Analytics
 
-        def _miss_rate(lut, sp, sf, size):
-            searched_res = []
-            used_tags = []
-            total_feature_count = defaultdict(int)
-            mismatched_feature_count = defaultdict(int)
-            for row in range(size):
-                searched_res.append(sp[row].split('|'))
-                used_tags.append(sf[row].split('|'))
-            for each_sp, each_sf in zip(searched_res, used_tags):
-                for each_props in each_sp:
-                    actual_features = set(lut[int(each_props)])
-                    searched_features = set(each_sf)
-                    for each in searched_features:
-                        total_feature_count[each] += 1
-                    for each in searched_features - actual_features:
-                        mismatched_feature_count[each] += 1
-            return dict(total_feature_count), dict(mismatched_feature_count)
+    >>> search_quality('property_features')
+    >>> search_quality('property_features', missing_rate=True)
+    """
+    prop_feat_list = data_property[what].apply(
+        lambda x: x.split('|')).values
+    lut = dict(list(zip(data_property.id.values, prop_feat_list)))
+    from collections import defaultdict
 
-        actual, mismatched = _miss_rate(
-            lut,
-            data_search.PropertiesReturned.values,
-            data_search.SearchKeywordsFeature.values,
-            data_search.shape[0])
-        miss_rate = defaultdict(float)
-        for each in actual:
-            miss_rate[each] = mismatched[each] / actual[each] * 100.0
+    def _miss_rate(lut, sp, sf, size):
+        searched_res = []
+        used_tags = []
+        total_feature_count = defaultdict(int)
+        mismatched_feature_count = defaultdict(int)
+        for row in range(size):
+            searched_res.append(sp[row].split('|'))
+            used_tags.append(sf[row].split('|'))
+        for each_sp, each_sf in zip(searched_res, used_tags):
+            for each_props in each_sp:
+                actual_features = set(lut[int(each_props)])
+                searched_features = set(each_sf)
+                for each in searched_features:
+                    total_feature_count[each] += 1
+                for each in searched_features - actual_features:
+                    mismatched_feature_count[each] += 1
+        return dict(total_feature_count), dict(mismatched_feature_count)
+
+    actual, mismatched = _miss_rate(
+        lut,
+        data_search.PropertiesReturned.values,
+        data_search.SearchKeywordsFeature.values,
+        data_search.shape[0])
+    miss_rate = defaultdict(float)
+    for each in actual:
+        miss_rate[each] = mismatched[each] / actual[each] * 100.0
+    if missing_rate:
         index = range(len(miss_rate))
         ret = preprocess.make_frame(miss_rate, index=index)
         ret = ret.iloc[0].reset_index()
         ret.columns = ['Features', 'Miss Rate']
-        output = lambda: ret.sort_values(by='Miss Rate',
-            ascending=False).set_index('Features')
+        output = lambda: ret.sort_values(
+            by='Miss Rate', ascending=False).set_index('Features')
         if Constants.DEBUG:
             import sys
             jwrap = json_wrap(override=sys.stdout)
@@ -252,4 +257,129 @@ def search_quality(what, miss_rate=False):
             jwrap = json_wrap(override=None)
             return True, jwrap.wrap(functor=output, indent=4)
     else:
-        pass
+        ret = {
+            'total_searches': data_search.shape[0],
+            'total_possible_matches': sum(actual.values()),
+            'missed_search': sum(mismatched.values())
+        }
+        output = lambda: ret
+        if Constants.DEBUG:
+            import sys
+            dwrap = dict_wrap(override=sys.stdout)
+            print(dwrap.wrap(functor=output))
+            return True, None
+        else:
+            dwrap = dict_wrap(override=None)
+            return True, dwrap.wrap(functor=output)
+
+
+def seller_portfolio(seller_id, metric):
+    """
+    Section 6: Seller Analytics
+    >>> seller_portfolio(seller_id, 'price_range')
+    >>> seller_portfolio(seller_id, 'number_of_features')
+    >>> seller_portfolio(seller_id, 'property_type')
+    >>> seller_portfolio(seller_id, buyer_engagement_time)
+    """
+    metrics = [
+        'price_range',
+        'number_of_features',
+        'property_type',
+        'buyer_engagement_time'
+
+    ]
+    if metric not in metrics:
+        if Constants.DEBUG:
+            err_msg = 'Mentioned metric {} is not implemented'.format(metric)
+            raise_(NotImplementedError, NotImplementedError(err_msg))
+    if metric == 'price_range':
+        if metric not in data_property.columns.values:
+            success = preprocess.create_col(
+                data=data_property,
+                col_name=metric,
+                used_cols=['listing_price'],
+                operation='CREATE_RANGE',
+                interval=1000000
+            )
+            if not success:
+                if Constants.DEBUG:
+                    print('Create Column operation was not successful')
+                return False, None
+        data_temp = data_property[data_property.added_by_id == seller_id].copy()
+        if data_temp.shape[0] == 0:
+            if Constants.DEBUG:
+                err_msg = 'seller_id: {} returned no entries'.format(seller_id)
+                raise_(ValueError, ValueError(err_msg))
+        ret = data_temp.groupby('price_range').id.count().reset_index()
+        ret.columns = ['price_range', 'total_count']
+        output = lambda: ret.set_index('price_range')
+        if Constants.DEBUG:
+            import sys
+            jwrap = json_wrap(override=sys.stdout)
+            print(jwrap.wrap(functor=output, indent=4))
+            return True, None
+        else:
+            jwrap = json_wrap(override=None)
+            return True, jwrap.wrap(functor=output, indent=4)
+    elif metric == 'number_of_features':
+        data_temp = data_property[data_property.added_by_id == seller_id].copy()
+        if data_temp.shape[0] == 0:
+            if Constants.DEBUG:
+                err_msg = 'seller_id: {} returned no entries'.format(seller_id)
+                raise_(ValueError, ValueError(err_msg))
+        ret = data_temp.sort_values(metric, ascending=False)
+        output = lambda: ret[['id', metric]].set_index('id')
+        if Constants.DEBUG:
+            import sys
+            jwrap = json_wrap(override=sys.stdout)
+            print(jwrap.wrap(functor=output, indent=4))
+            return True, None
+        else:
+            jwrap = json_wrap(override=None)
+            return True, jwrap.wrap(functor=output, indent=4)
+    elif metric == 'property_type':
+        data_temp = data_property[data_property.added_by_id == seller_id].copy()
+        if data_temp.shape[0] == 0:
+            if Constants.DEBUG:
+                err_msg = 'seller_id: {} returned no entries'.format(seller_id)
+                raise_(ValueError, ValueError(err_msg))
+        ret = data_temp.groupby(metric).id.count().reset_index()
+        ret.columns = ['id', 'property_type']
+        output = lambda: ret.set_index('id')
+        if Constants.DEBUG:
+            import sys
+            jwrap = json_wrap(override=sys.stdout)
+            print(jwrap.wrap(functor=output, indent=4))
+            return True, None
+        else:
+            jwrap = json_wrap(override=None)
+            return True, jwrap.wrap(functor=output, indent=4)
+    else:
+        preprocess.extract_timeseries(
+            data=data_visits,
+            ts_col='VisitingTimeStart')
+        preprocess.extract_timeseries(
+            data=data_visits,
+            ts_col='VisitingTimeEnd')
+        preprocess.generic_operations(
+            data_visits,
+            'VisitingTimeEnd',
+            'VisitingTimeStart',
+            'spent',
+            '-')
+        preprocess.extract_timeseries(
+            data=data_visits,
+            into='minutes',
+            ts_col='spent')
+        ret = data_visits.groupby('PropertyID').minutes.mean()
+        ret = ret.reset_index()
+        ret.sort_values(by='minutes', ascending=False, inplace=True)
+        output = lambda: ret.set_index('PropertyID')
+        if Constants.DEBUG:
+            import sys
+            jwrap = json_wrap(override=sys.stdout)
+            print(jwrap.wrap(functor=output, indent=4))
+            return True, None
+        else:
+            jwrap = json_wrap(override=None)
+            return True, jwrap.wrap(functor=output, indent=4)
